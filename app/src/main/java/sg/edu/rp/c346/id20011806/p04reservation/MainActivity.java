e065a0d1881f9a21edcfedc6f590ed41cdabdefe
20011806 <20011806@myrp.edu.sg>
2021-05-18 09:06:26 +0800

    z

--- app/src/main/java/sg/edu/rp/c346/id20011806/p04reservation/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20011806/p04reservation/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.sql.Time;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,19 +47,25 @@ public class MainActivity extends AppCompatActivity {
         submitBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Context context = getApplicationContext();
-                String text = "Thank you for reserving with us " + cusName.getText().toString();
-                if (smokingTable.getCheckedRadioButtonId() == R.id.smokingBtn) {
-                    text += " you have reserved a smoking table";
+                if (cusName.getText().toString().length() == 0 || mobileNum.getText().toString().length() == 0 || groupSize.getText().toString().length() == 0) {
+                    Context context = getApplicationContext();
+                    String text = "Please fill in your details";
+                    Toast.makeText(context, text, Toast.LENGTH_SHORT);
                 } else {
-                    text += " you have reserved a non-smoking table";
-                }
-                text += " for " + groupSize.getText().toString() + " people";
-                text += " under the contact number " + mobileNum.getText().toString();
-                text += " at " + String.format("%d:%02d", tp.getCurrentHour(), tp.getCurrentMinute());
-                text += " on " + dp.getDayOfMonth() + "/" + (dp.getMonth()+1) + "/" + dp.getYear();
+                    Context context = getApplicationContext();
+                    String text = "Thank you for reserving with us " + cusName.getText().toString();
+                    if (smokingTable.getCheckedRadioButtonId() == R.id.smokingBtn) {
+                        text += ", you have reserved a smoking table";
+                    } else {
+                        text += ", you have reserved a non-smoking table";
+                    }
+                    text += " for " + groupSize.getText().toString() + " people";
+                    text += " under the contact number " + mobileNum.getText().toString();
+                    text += " at " + String.format("%d:%02d", tp.getCurrentHour(), tp.getCurrentMinute());
+                    text += " on " + dp.getDayOfMonth() + "/" + (dp.getMonth()+1) + "/" + dp.getYear();
 
-                Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -76,5 +83,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        tp.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker v, int hourOfDay, int minute){
+                if (hourOfDay > 20) {
+                    tp.setCurrentHour(20);
+                } else if (hourOfDay < 8) {
+                    tp.setCurrentHour(8);
+                }
+            }
+        });
+
+        Calendar todaysDate = Calendar.getInstance();
+        int currentYear = todaysDate.get(Calendar.YEAR);
+        int currentMonth = todaysDate.get(Calendar.MONTH);
+        int currentDay = todaysDate.get(Calendar.DAY_OF_MONTH);
+
+        dp.updateDate(currentYear,currentMonth,currentDay+1);
+        tp.setCurrentMinute(30);
+        tp.setCurrentHour(19);
+        dp.setMinDate(System.currentTimeMillis() + 86400000);
     }
 }
